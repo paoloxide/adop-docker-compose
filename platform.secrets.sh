f5c92cd84009a90ed522e3120bb5e5739be22874
EC2 Default User <ec2-user@ip-172-31-16-103.eu-west-1.compute.internal>
2016-06-14 07:29:59 +0000

    Updated the jenkins docker image to jenkins-siebel:0.1

---
+++ platform.secrets.sh
@@ -0,0 +1,16 @@
+# Example file to store all the generated secrets for your ADOP stack
+
+# Username for initial admin user
+export INITIAL_ADMIN_USER="paolo"
+
+# Password for initial admin user
+export INITIAL_ADMIN_PASSWORD_PLAIN="3b709146daba6762"
+
+# Password for the Jenkins user
+export PASSWORD_JENKINS="1e6a8dd7fe461209"
+
+# Password for the Gerrit user
+export PASSWORD_GERRIT="127893c312264915"
+
+# Root password for the Sonar and Gerrit MySQL instances
+export PASSWORD_SQL="65c25aa649ac5214"
